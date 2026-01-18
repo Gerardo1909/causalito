4a040f74fe94327e100200ffd547ea3fe8494bb7
Gerardo1909 <gerardotoboso1909@gmail.com>
2026-01-18 16:09:44 -0300

    feat(src/agent/rag_agent.py): Se agrega clase que centraliza el flujo del agente RAG.

---
+++ src/agent/rag_agent.py
@@ -0,0 +1,38 @@
+"""
+Módulo que contiene la lógica de orquestación del RAG.
+"""
+
+from typing import List, Tuple
+
+from agent.prompt_builder import PromptBuilder
+from llm.base import LLM
+from retrieval.retriever import Retriever
+
+
+class RAGAgent:
+    """
+    Clase que implementa un agente de Recuperación-Augmentada por Generación (RAG).
+    """
+
+    def __init__(self, retriever: Retriever, llm: LLM, prompt_builder: PromptBuilder):
+        self.retriever = retriever
+        self.llm = llm
+        self.prompt_builder = prompt_builder
+
+    def answer(self, question: str) -> Tuple[str, List[str]]:
+        """
+        Docstring for answer
+
+        :param self: Description
+        :param question: Description
+        :type question: str
+        :return: Description
+        :rtype: Tuple[str, list[str]]
+        """
+        docs, sources = self.retriever.retrieve(question)
+
+        if not docs:
+            return "No hay suficiente información en los documentos para responder.", []
+
+        prompt = self.prompt_builder.build(question, docs)
+        return self.llm.generate(prompt), sources
